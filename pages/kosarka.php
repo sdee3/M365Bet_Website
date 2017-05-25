ac9b111d273ac57a4412a2ecfe15af4e15a8fa69
Stefan Djokic <stefandjokic@wegrowth.com>
2017-05-25 19:20:25 +0200

    img rename, added other images to pages

--- pages/kosarka.php
+++ pages/kosarka.php
@@ -13,6 +13,20 @@
 
    <body style = "font-family: 'PT Sans', sans-serif;">
 
+     <div class="container" style="margin-top: 6em; margin-bottom: 6em;">
+
+       <img width="75%" style="display: block; margin: 0 auto;" class="img-responsive thumbnail" src="../img/basketball.jpg" alt="Kosarka Mokranjatzz Mokranjatzz365Bet">
+       <br><br>
+
+       <div class="container container-fluid">
+         <p>Poštovani posetioče, ova igra ili funkcija Web portala Mokranjatzz kladionice je trenutno u izradi. Naš tim stručnjaka aktivno
+         radi kako bi doneo sveže novine u sistem najperspektivnije kladionice do sad.</p>
+         <p>U međuvremenu, okušajte svoju sreću na ekskluzivnoj Lazarevačkoj drugoj ligi (<a href="../pages/fudbal.php" target="_blank">fudbal</a>), gde
+         Vas čeka mnogo toga!</p>
+         <p>Hvala na razumevanju. Vaš Mokranjatzz365Bet.</p>
+       </div>
+
+     </div>
 
      <?php include_once('../includes/footer.php'); ?>
   </body>
--- pages/tenis.php
+++ pages/tenis.php
@@ -9,11 +9,24 @@
       $currentPage = 'tenis';
       include_once('../includes/layout.php');
       include_once('../includes/navbar.php');
-      include_once('../includes/footer.php');
   ?>
 
    <body style = "font-family: 'PT Sans', sans-serif;">
 
+     <div class="container" style="margin-top: 6em; margin-bottom: 6em;">
+
+      <img width="75%" style="display: block; margin: 0 auto;" class="img-responsive thumbnail" src="../img/tennis.jpg" alt="Tenis Mokranjatzz Mokranjatzz365Bet">
+      <br><br>
+
+       <div class="container container-fluid">
+         <p>Poštovani posetioče, ova igra ili funkcija Web portala Mokranjatzz kladionice je trenutno u izradi. Naš tim stručnjaka aktivno
+         radi kako bi doneo sveže novine u sistem najperspektivnije kladionice do sad.</p>
+         <p>U međuvremenu, okušajte svoju sreću na ekskluzivnoj Lazarevačkoj drugoj ligi (<a href="../pages/fudbal.php" target="_blank">fudbal</a>), gde
+         Vas čeka mnogo toga!</p>
+         <p>Hvala na razumevanju. Vaš Mokranjatzz365Bet.</p>
+       </div>
+
+     </div>
 
      <?php include_once('../includes/footer.php'); ?>
   </body>
